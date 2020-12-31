104ffb18c6b806dc54a69875a814997e4bacedff
netphar <forfimm2018@gmail.com>
2020-12-31 11:15:06 +0000

    add prediction script for 13 different regression models

--- mymodule/predict_synergy.py
+++ mymodule/predict_synergy.py
@@ -78,4 +78,107 @@ def run_train(file, fp_name, cv=10, for_valid=0.4, ordered = False, ram_fraction
                 with open(nm, 'wb') as file:
                     pickle.dump(c, file)
     return c
-   
+
+def run_train_all_sklearn(file, fp_name, cv=5, verbose=0, seed=1):
+    
+    np.random.seed(seed)
+    c = defaultdict(list)
+    
+    for k in ProgIter(['synergy_zip', 'synergy_bliss', 'synergy_loewe', 'synergy_hsa','css_ri','name'], verbose=verbose, total=5):
+        v = file[k]
+        
+        if k != 'name':
+            temp = dict() # for results storage. Assuming that "name" comes last
+            
+            if 'drug_row_col' in v.columns:
+                v.drop(columns=['drug_row_col'], inplace=True)
+            
+            cat_cols = ['cell_line_name']
+            categories = [v[column].unique() for column in v[cat_cols]] # manually find all available categories for one-hot
+            
+            # pipelines
+            encode = Pipeline(steps=[
+                ('one-hot-encode', OneHotEncoder(categories=categories))
+            ])
+            processor = ColumnTransformer(
+                transformers=[
+                    ('cat_encoding', encode, cat_cols),
+                    ('dropping', 'drop', [k]) 
+                ],
+                remainder='passthrough')
+            
+            catbst = ColumnTransformer(
+                transformers=[
+                    ('dropping', 'drop', [k]) 
+                ],
+                remainder='passthrough')
+            
+            # regressions
+            lr = make_pipeline(processor, linear_model.LinearRegression())
+            ridge = make_pipeline(processor, linear_model.Ridge())
+            lasso = make_pipeline(processor, linear_model.Lasso())
+            elastic = make_pipeline(processor, linear_model.ElasticNet())
+            lassolars = make_pipeline(processor, linear_model.LassoLars())
+            b_ridge = make_pipeline(processor, linear_model.BayesianRidge())
+            kernel = DotProduct() + WhiteKernel()
+            gpr = make_pipeline(processor, GaussianProcessRegressor(kernel=kernel))
+            linSVR = make_pipeline(processor, LinearSVR())
+            hist_gbr = make_pipeline(processor, HistGradientBoostingRegressor(warm_start=True, 
+                                                                              max_depth=6))
+            rfr = make_pipeline(processor, RandomForestRegressor(warm_start=True, 
+                                                                 max_depth=6, 
+                                                                 n_jobs=3))
+            iso = make_pipeline(processor, IsotonicRegression(increasing='auto'))
+            xgb = make_pipeline(processor, XGBRegressor(tree_method='gpu_hist', 
+                                                        max_depth=6))
+            cbt = make_pipeline(catbst, CatBoostRegressor(task_type='GPU', 
+                                                          depth=6, 
+                                                          cat_features=np.array([0]), 
+                                                          verbose=False))
+            
+            mls = [cbt, rfr, gpr, hist_gbr,
+                   lr, ridge, lasso, elastic, lassolars, 
+                   b_ridge, 
+                   gpr, 
+                   linSVR, 
+                   iso]
+            mls_names = ["cbt","rfr","gpr", "hist_gbr",
+                         "lr", "ridge","lasso","elastic","lassolars", 
+                         "b_ridge", 
+                         "gpr", 
+                         "linSVR", 
+                         "iso"]
+            
+            # results 
+            start = time.time()
+            for MODEL, name in zip(mls, mls_names):
+                print(f'\n{name}')
+                if 'cbt' == name:
+                    n_jobs = 1
+                else:
+                    n_jobs = cv
+                cv_dict = cross_validate(
+                    MODEL,
+                    v, 
+                    v[k],
+                    cv=cv, 
+                    scoring={"pearsonr":pearson,
+                             "rmse":rmse}, 
+                    return_train_score=False, 
+                    verbose=verbose,
+                    n_jobs=n_jobs, 
+                )
+                temp[name] = {
+                    'test_pearsonr' : np.nanmean(cv_dict['test_pearsonr']), 
+                    'test_rmse' : abs(np.nanmean(cv_dict['test_rmse']))
+                }
+                print(temp[name])
+            print(f'{k} took {int(time.time()-start)/60} mins')
+            
+            c[k] = temp
+        else:
+            nm=f'/tf/notebooks/code_for_pub/_logs_as_python_files/{fp_name}_13models_5foldCV_{time.ctime()}.pickle'
+            with open(nm, 'wb') as file:
+                pickle.dump(c, file)
+            print(f'saving complete to {nm}')
+    return c
